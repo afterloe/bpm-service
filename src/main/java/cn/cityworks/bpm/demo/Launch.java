a0427dac48401f460bf81ba7e42ff366c42a9ab7
afterloe <605728727@qq.com>
2017-10-11 21:54:18 +0800

    spring boot 项目初始化

--- src/main/java/cn/cityworks/bpm/demo/Launch.java
+++ src/main/java/cn/cityworks/bpm/demo/Launch.java
@@ -1,8 +1,15 @@
 package cn.cityworks.bpm.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by afterloe on 10/11/2017.
  */
-
+@SpringBootApplication
 public class Launch {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Launch.class, args);
+    }
 }
