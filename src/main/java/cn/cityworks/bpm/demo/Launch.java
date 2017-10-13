d9f7404d5f9df5192e15963c7dcb6da4270e4ab6
afterloe <lm6289511@gmail.com>
2017-10-13 16:44:27 +0800

    任务列表

--- src/main/java/cn/cityworks/bpm/demo/Launch.java
+++ src/main/java/cn/cityworks/bpm/demo/Launch.java
@@ -2,11 +2,13 @@ package cn.cityworks.bpm.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  * Created by afterloe on 10/11/2017.
  */
 @SpringBootApplication
+@EnableFeignClients
 public class Launch {
 
     public static void main(String[] args) {
