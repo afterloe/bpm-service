627131386205bbb0abd13d6438184104f3357701
afterloe <605728727@qq.com>
2017-10-14 00:20:36 +0800

    智能提醒缺省参数

--- src/main/java/cn/cityworks/bpm/demo/services/impl/BPMServiceImpl.java
+++ src/main/java/cn/cityworks/bpm/demo/services/impl/BPMServiceImpl.java
@@ -7,11 +7,13 @@ import org.activiti.engine.form.FormProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * create by afterloe on 2017/10/13
@@ -33,6 +35,14 @@ public class BPMServiceImpl implements BPMService {
     public Object startProcess(String processId, Map formData, String access_token) {
         List<FormProperty> items = formService.getStartFormData(processId).getFormProperties();
         Map<String, String> variables = new HashMap<>();
+        FormProperty lackParameter = items.stream()
+                .filter(item -> item.isRequired() && !formData.containsKey(item.getId()))
+                .findAny()
+                .orElse(null);
+        if (null != lackParameter) {
+            throw BasicException.build("lack parameter -> " + lackParameter.getId(),
+                    HttpStatus.BAD_REQUEST.value());
+        }
         items.stream().forEach(item -> {
             String key = item.getId();
             variables.put(key, formData.get(key).toString());
